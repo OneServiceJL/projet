e1d22cb7a47294268301b0b26664107e0d0cc908
OneServiceJL <oneservice_2025@outlook.com>
2025-05-20 19:08:17 +0200

    nouveau front and

--- pages/includes/sidebar.php
+++ pages/includes/sidebar.php
@@ -14,16 +14,114 @@
                 <!-- Top 5 Sai (Studio Production) -->
                 <li class="submenu">
                     <a href="javascript:void(0);">
-                        <i class="fa fa-film" aria-hidden="true"></i>
-                        <span>Top 5 Sai</span>
+                        <i class="fa fa-music" aria-hidden="true"></i>
+                        <span>TOP 5 MUSIC</span>
                         <span class="menu-arrow"></span>
                     </a>
                     <ul>
-                        <li><a href="studio-bookings.php">Studio Bookings</a></li>
-                        <li><a href="production-schedule.php">Production Schedule</a></li>
-                        <li><a href="equipment-usage.php">Equipment Usage</a></li>
-                        <li><a href="recording-sessions.php">Recording Sessions</a></li>
-                        <li><a href="studio-reports.php">Studio Reports</a></li>
+                        <!-- Département Artistique & A&R -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-user-plus" aria-hidden="true"></i>
+                                <span>Artistique & A&R</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/artistique/artist-discovery.php">Découverte d'artistes</a></li>
+                                <li><a href="TOP_5_MUSIC/artistique/artist-development.php">Développement artistique</a></li>
+                                <li><a href="TOP_5_MUSIC/artistique/artist-database.php">Base données artistes</a></li>
+                                <li><a href="TOP_5_MUSIC/artistique/production-calendar.php">Calendrier de productions</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Production Musicale -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-headphones" aria-hidden="true"></i>
+                                <span>Production Musicale</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/production/studio-bookings.php">Réservation studio</a></li>
+                                <li><a href="TOP_5_MUSIC/production/recording-sessions.php">Sessions d'enregistrement</a></li>
+                                <li><a href="TOP_5_MUSIC/production/beat-library.php">Librairie de beats</a></li>
+                                <li><a href="TOP_5_MUSIC/production/copyright-management.php">Gestion des droits</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Marketing & Promotion -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                                <span>Marketing & Promotion</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/digital/digital-campaigns.php">Campagnes digitales</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/media-kits.php">Kits médias</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/streaming-analytics.php">Analytics streaming</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/social-media.php">Réseaux sociaux</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Événements & Tournées -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                <span>Événements & Tournées</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/evenements/concert-booking.php">Booking concerts</a></li>
+                                <li><a href="TOP_5_MUSIC/evenements/tour-schedule.php">Planning tournées</a></li>
+                                <li><a href="TOP_5_MUSIC/evenements/event-budget.php">Budgets événements</a></li>
+                                <li><a href="TOP_5_MUSIC/evenements/venue-database.php">Base données salles</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Digital & Plateformes -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-digital-tachograph" aria-hidden="true"></i>
+                                <span>Digital & Plateformes</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/digital/streaming-optimization.php">Optimisation streaming</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/platform-dashboard.php">Dashboard plateformes</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/seo-management.php">Gestion SEO</a></li>
+                                <li><a href="TOP_5_MUSIC/digital/content-strategy.php">Stratégie de contenu</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Juridique -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-gavel" aria-hidden="true"></i>
+                                <span>Juridique & Droits</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/juridique/contracts-management.php">Gestion des contrats</a></li>
+                                <li><a href="TOP_5_MUSIC/juridique/royalties-distribution.php">Distribution royalties</a></li>
+                                <li><a href="TOP_5_MUSIC/juridique/copyright-disputes.php">Litiges droits d'auteur</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Département Analytics -->
+                        <li class="submenu">
+                            <a href="javascript:void(0);">
+                                <i class="fa fa-chart-line" aria-hidden="true"></i>
+                                <span>Analytics & Data</span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul>
+                                <li><a href="TOP_5_MUSIC/analytics/music-trends.php">Trends musicales</a></li>
+                                <li><a href="TOP_5_MUSIC/analytics/revenue-reports.php">Rapports revenus</a></li>
+                                <li><a href="TOP_5_MUSIC/analytics/fan-demographics.php">Démographie fans</a></li>
+                                <li><a href="TOP_5_MUSIC/analytics/success-predictions.php">Prédictions de succès</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </li>
                 
