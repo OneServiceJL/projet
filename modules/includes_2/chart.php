ca9293b09fc9d0917a083768f256708b21651620
OneServiceJL <oneservice_2025@outlook.com>
2025-05-28 06:53:42 +0200

    feat: add dashboard page with multiple chart visualizations

--- modules/includes_2/chart.php
+++ modules/includes_2/chart.php
@@ -102,20 +102,13 @@
         <!-- jQuery -->
         <script src="assets/js/jquery-3.7.1.min.js"></script>
 
-        <!-- Feather Icon JS -->
-        <script src="assets/js/feather.min.js"></script>
+       
 
-        <!-- Slimscroll JS -->
-        <script src="assets/js/jquery.slimscroll.min.js"></script>
-
-        <!-- Bootstrap Core JS -->
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
+       
 
         <!-- Chart JS -->
 		<script src="assets/plugins/c3-chart/d3.v5.min.js"></script>
 		<script src="assets/plugins/c3-chart/c3.min.js"></script>
 		<script src="assets/plugins/c3-chart/chart-data.js"></script>
         
-        <!-- Custom JS -->
-		<script src="assets/js/theme-script.js"></script>
-		<script src="assets/js/script.js"></script>
+        
